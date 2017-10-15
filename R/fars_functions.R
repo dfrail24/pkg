afff86cab7874159f1afe1b35bec741e561d5518
Frail Dayae <FrailDaYae@JohnDeere.com>
2017-10-14 20:38:35 -0500

    Commit package

---
+++ R/fars_functions.R
@@ -0,0 +1,115 @@
+
+#' FARS data import
+#'
+#' Reads in FARS data
+#' @param filename File (CSV) name
+#' @return Data frame table
+#' @export
+#' @examples \dontrun{
+#' fars_read("accident_2013.csv")
+#' }
+#'
+
+fars_read <- function(filename) {
+  if(!file.exists(filename))
+    stop("file '", filename, "' does not exist")
+  data <- suppressMessages({
+    readr::read_csv(filename, progress = FALSE)
+  })
+  dplyr::tbl_df(data)
+}
+
+
+#'Create filename
+#'
+#'Create filename for FARS data
+#'@param year Year that data were collected
+#'@return Filename
+#'@export
+#'@examples \dontrun{
+#'make_filename(2017)
+#'}
+#'
+
+make_filename <- function(year) {
+  year <- as.integer(year)
+  sprintf("accident_%d.csv.bz2", year)
+}
+
+
+#' Read Multiple Files
+#'
+#' Read multiple files and confirm that the years are valid
+#' @param years Years that data were collected
+#' @return List of data frames (NULL if data do not exist)
+#' @export
+#' @examples \dontrun{
+#' fars_read_years(2013:2015)
+#' }
+#'
+
+fars_read_years <- function(years) {
+  lapply(years, function(year) {
+    file <- make_filename(year)
+    tryCatch({
+      dat <- fars_read(file)
+      dplyr::mutate(dat, year = year) %>%
+        dplyr::select(MONTH, year)
+    }, error = function(e) {
+      warning("invalid year: ", year)
+      return(NULL)
+    })
+  })
+}
+
+#' Summarize data
+#'
+#' Summarize yearly data by month
+#' @param years Years that data were collected
+#' @return Summary
+#' @export
+#' @examples \dontrun{
+#' fars_summarize_years(2013:2015)
+#' }
+#'
+
+fars_summarize_years <- function(years) {
+  dat_list <- fars_read_years(years)
+  dplyr::bind_rows(dat_list) %>%
+    dplyr::group_by(year, MONTH) %>%
+    dplyr::summarize(n = n()) %>%
+    tidyr::spread(year, n)
+}
+
+
+#' Create map
+#'
+#' Create map visualization of FARS data
+#' @param state.num State number
+#' @param year Year that data were collected
+#' @return Visual map
+#' @export
+#' @examples \dontrun{
+#' fars_map_state(1, 2013)
+#' }
+
+fars_map_state <- function(state.num, year) {
+  filename <- make_filename(year)
+  data <- fars_read(filename)
+  state.num <- as.integer(state.num)
+
+  if(!(state.num %in% unique(data$STATE)))
+    stop("invalid STATE number: ", state.num)
+  data.sub <- dplyr::filter(data, STATE == state.num)
+  if(nrow(data.sub) == 0L) {
+    message("no accidents to plot")
+    return(invisible(NULL))
+  }
+  is.na(data.sub$LONGITUD) <- data.sub$LONGITUD > 900
+  is.na(data.sub$LATITUDE) <- data.sub$LATITUDE > 90
+  with(data.sub, {
+    maps::map("state", ylim = range(LATITUDE, na.rm = TRUE),
+              xlim = range(LONGITUD, na.rm = TRUE))
+    graphics::points(LONGITUD, LATITUDE, pch = 46)
+  })
+}
