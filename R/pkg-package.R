d89128f41c228b3dbbf32e9cc8733d94dbe3eb26
Frail Dayae <FrailDaYae@JohnDeere.com>
2017-10-14 21:26:06 -0500

    Changes to imported packages

---
+++ R/pkg-package.R
@@ -0,0 +1,10 @@
+#' Package for FARS Data
+#'
+#' @name pkg
+#' @docType package
+#' @author Dayae Frail (dfrail24@@outlook.com)
+#' @import dplyr
+#' @importFrom maps map
+#' @importFrom readr read_csv
+#' @importFrom tidyr spread
+NULL
